dc551748998ad7c3b17e4e49c8a2a7305c28fc8f
Milind Ramdas Diwane <milind.d@itp.objectfrontier.com>
2021-04-25 01:28:18 +0530

    updated frontend to understand the flow

--- demo.py
+++ demo.py
@@ -6,15 +6,15 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
+        'author': 'xyz',
+        # 'title': 'Blog Post 1',
+        'content': '1st post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
+        'author': 'Abc',
+        # 'title': 'Blog Post 2',
+        'content': '2nd post content',
         'date_posted': 'April 21, 2018'
     }
 ]
@@ -23,7 +23,18 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',title='home', posts=posts,inside='true')
+    connections=[
+    {
+        'name': 'Abc Schafer',
+    },
+    {
+        'name': 'Pqr Doe',
+    },
+    {
+        'name': 'Xyz -',
+    }
+]
+    return render_template('home.html',title='home', posts=posts,inside='true',connections=connections)
 
 
 @app.route("/about")
@@ -53,11 +64,23 @@ def login():
 
 @app.route("/profile", methods=['GET', 'POST'])
 def profile():
+    connections=[
+    {
+        'name': 'Abc Schafer',
+    },
+    {
+        'name': 'Pqr Doe',
+    },
+    {
+        'name': 'Xyz -',
+    }
+]
+
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Profile created for {form.username.data}!', 'success')
         return redirect(url_for('profile'))
-    return render_template('profile.html', title='Profile',inside='true', form=form)
+    return render_template('profile.html', title='Profile',inside='true', form=form,connections=connections, posts=posts)
 
 @app.route("/notifications")
 def notifications():
@@ -67,8 +90,21 @@ def notifications():
 
 @app.route("/connections")
 def connections():
+    connections=[
+    {
+        'name': 'Abc Schafer',
+    },
+    {
+        'name': 'Pqr Doe',
+    },
+    {
+        'name': 'Xyz -',
+    }
+]
 
-    return render_template('connections.html', title='connections',inside='true')
+    pending_requests = [{'name':'Name1 Sirname1'},{'name':'Name2 Sirname2'},{'name':'Name3 Sirname3'}]
+
+    return render_template('connections.html', title='connections',inside='true',pending_requests=pending_requests,connections=connections)
 
 
 if __name__ == '__main__':
